ac60a51bfa42bb39cc8da42f6e0fd8c21a3f4e39
Lanka Saran Manikanta swamy <saranlanka033@gmail.com>
2026-03-20 14:45:19 +0530

    Update Vite config for React plugin and build settings
    Updated Vite configuration to use the standard React plugin and added build output directory.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/cute-class-register/',
   plugins: [react()],
-});
+  build: {
+    outDir: 'dist',
+  }
+})
