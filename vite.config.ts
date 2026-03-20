753f80752791799134152f0da93d3362e0becd58
Lanka Saran Manikanta swamy <saranlanka033@gmail.com>
2026-03-20 14:43:36 +0530

    Remove unnecessary comment from vite.config.ts
    Removed comment about the simplicity of the config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// This is the simplest possible config to get your site live
 export default defineConfig({
   base: '/cute-class-register/',
   plugins: [react()],
