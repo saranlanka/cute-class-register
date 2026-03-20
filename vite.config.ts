c0eb20c472f0fea22c23c009e36179ca8658993a
Lanka Saran Manikanta swamy <saranlanka033@gmail.com>
2026-03-20 14:54:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// This version is designed to work even if your folder structure is complex
 export default defineConfig({
   base: '/cute-class-register/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false
-  }
 })
